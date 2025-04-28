0b45ec7e8e853c6d8dfad2efe6ccd0778f728b4a
RaynaTalia <Raynataliaw@gmail.com>
2025-04-27 21:42:30 -0400

    Update loginController.java

--- loginController.java
+++ loginController.java
@@ -1,1 +1,46 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
+
+public class loginController {
+  // create labels, text feilds, and buttons 
+    @FXML
+    private Label username;
+    @FXML
+    private Label password;
+    @FXML
+    private TextField userField;
+    @FXML
+    private PasswordField passField;
+    @FXML
+    private Button loginButton;
+    @FXML
+  // actions when login button is clicked, if information is correct  moves to the next screen else an error message is displayed 
+    private void handleLoginButton()
+    {
+        String user = userField.getText();
+        String pass = passField.getText();
+        if(user.equals("Kimberly J") && pass.equals("kim123")) || user.equals("John S") && pass.equals("john123")|| user.equals("Bob M") && pass.equals("bob123")
+        {
+           FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
+            Parent tableStatusRoot = loader.load();
+
+            // Get the current stage (window)
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+
+            // Set the new Scene (Table Status screen)
+            Scene tableScene = new Scene(tableStatusRoot);
+            stage.setScene(tableScene);
+
+        } catch (Exception e) {
+        e.printStackTrace();
+    }
+    } else {
+        // Show error message if wrong login
+        System.out.println("Invalid login, please try again.");
+    }
+        }
+        }
+}
